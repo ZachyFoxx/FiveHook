68944002fa03eeeab5d6d1e7fe6e37139e47ffea
Zachery Elliot <notzachery@gmail.com>
2023-02-20 07:40:53 -0600

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'Better Tire Slash'
+description 'An enhanced version of slashtires by xander1998'
+author 'ZachyFoxx'
+url 'https://github.com/ZachyFoxx/5m_better_tire_slash'
+
+client_script 'client/src/client.js'
+server_script 'server/src/server.js'
